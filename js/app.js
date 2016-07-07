e8a7a9f1c50726ad11ee1f0986090ccf380963b5
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-07-06 20:57:09 -0700

    resetValues function added, event handler for New Game corrected

--- js/app.js
+++ js/app.js
@@ -12,23 +12,54 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});	
 
-	/** ---COMPLETED---
-		 * randomNumber() generates a random number
-		 * b/w 1 and 100.
-		 */
+	/*--- RUNS NEWGAME FUNCTION WHEN DOCUMENT LOADS ---*/	
+		
+    runGame();   
+
+	/*--- RUN GAME FUNCTION ---*/
+
+		function runGame() {
+
+			//--- RESETS VALUES TO DEFAULT
+			// resetValues();
+
+			//--- CHOOSES A RANDOM NUMBER AS THE ANSWER
+			var correctAnswer = randomNumber();
+			console.log(correctAnswer);
+			
+			// RESETS GAME TO DEFAULTS WHEN USER CLICKS 'NEW GAME'  
+
+			$('nav .new').click(function() {
+				resetValues();
+				correctAnswer = randomNumber();
+				console.log(correctAnswer);
+			});
+
+			//--- USER CLICKS TO SUBMIT GUESS 
+			$('#guessButton').click(function(event) {
+				validate(($('#userGuess').val()), correctAnswer);
+				$('#userGuess').val('');
+			});	
+
+		}
+
+	/*--- RESET VALUES FUNCTION ---*/
+
+		function resetValues() {
+			$('h2#feedback').text('Make Your Guess!');
+			$('#guessList').empty();
+			$('#count').text(0);
+			return randomNumber();
+		}
+
+  /*--- RANDOM NUMBER GENERATOR ---*/
+
 		function randomNumber() {
 			return Math.ceil(Math.random() * 100);
 		}
 
 	/*--- TESTS TO BE RUN IN VALIDATE FUNCTION ---*/
 
-		/** ---COMPLETED---
-		 * checkHistoryFor() Checks to see if a number 
-		 * 		is in ul#guessList
-		 * parameter: number being searched for
-		 * returns: FALSE if number is found.
-		 * 					TRUE if not found.
-		 */
 		function checkGuessListFor(number) {
 			guessListItems = [];
 			
@@ -46,12 +77,6 @@ $(document).ready(function(){
 			return true;
 		}
 		
-		/** ---COMPLETED---
-		 * testGuess() checks that a guessValue
-		 * falls within 1 and 100 AND is a whole number.
-		 * returns: FALSE if criteria are not met
-		 * 					TRUE otherwise
-		 */
 		function testGuess(guess) {
 			guess = parseFloat(guess);
 
@@ -65,19 +90,6 @@ $(document).ready(function(){
 
 	/*--- FUNCTIONS THAT RUN IF ANSWER PASSES VALIDATION ---*/
 
-		/** ---COMPLETED---
-		 * guessEvaluate() compares the user's guess to the 
-		 * correct answer and returns a statement about how far
-		 * user is from correct answer.
-		 * parameter: userGuess, correctAnswer
-		 * return:
-		 *		If user guesses correct number
-		 *		If user is < 5 from number: 'Very hot'
-		 *		If user is < 10 from number: 'Hot' 
-		 *		If user is < 20 from number: 'Kinda hot 
-		 *		If user is < 50 from number: 'Cold' 
-		 *		If user is > 50 from number: 'Very Cold' 
-		 */
 		function guessFeedback(guess, correctAnswer) {
 			var guessDifference = Math.abs(correctAnswer - guess);
 
@@ -109,17 +121,11 @@ $(document).ready(function(){
 			return number + 1;
 		}
 
-
   /*--- VALIDATION FUNCTION ---*/
 
-		/** ---COMPLETED---
-			 * validate() ensures user's guess is a valid
-			 * 		option.
-			 * parameter: userGuess, correctAnswer
-			 * returns FALSE if user's guess is not a valid option;
-			 * runs the game if user's guess is a valid option
-			 */
 		function validate(guess, correctAnswer) {
+			console.log(testGuess(guess));
+			console.log(guess);
 			if(testGuess(guess) === false) {
 				alert('Please choose a valid number');
 			} else if (checkGuessListFor(guess) === false) {
@@ -132,40 +138,11 @@ $(document).ready(function(){
 		 		addListItem('ul#guessList', guess);
 				changeText('span#count', addOne($('span#count').text()));
 			}
+
+
 		}
 
-	/*--- USER CLICKS TO SUBMIT GUESS ---*/
-		
-		$('#guessButton').click(function(event) {
-			validate($('#userGuess').val(), 30);
-			$('#userGuess').val('');
-		});
 
-
-		/** ---TO DO---
-		 * guessingGame() runs the guessing Game
-		 */
-		function guessingGame(userGuess, correctAnswer) {
-			//Checks guess for validity using validateGuess()
-			//adds 1 to counter (span#count)
-	  }
-
-  /** ---TO DO---
-	 * Starts New Game
-   */
-	function newGame() {
-  	/*--- Resets all game values to default ---*/
-  	function resetGame() {
-	  /*--- Sets h2#feedback to 'Make your Guess!' ---*/
-	  /*--- Sets span#count to '0' ---*/
-	  /*--- Clears ul#guesslist of previous guesses ---*/	
-		} 
-		/*--- randomNumber function 
-				* stored in a variable for reuse 
-		---*/
-	  /*---Runs the guessing Game ---*/
-	  /*--- Freezes Guessing Game if user has already won ---*/
-	}
 });
 
 
